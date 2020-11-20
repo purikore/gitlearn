8b1e7dcb65729854c12ed26bdbe68dc75afad12b
liujia <15096318060@163.com>
2020-11-20 22:03:09 +0800

    read_write.c

--- read_write.c
+++ read_write.c
@@ -1,16 +1,30 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <unistd.h>
 pthread_rwlock_t rwlock;
-
-void * readr(void * argv)
+int data = 0;
+void * reader(void * argv)
 {
-	
+	while(1)
+	{
+		pthread_rwlock_rdlock(&rwlock);
+		printf("子进程读到%d\n", data);
+		pthread_rwlock_unlock(&rwlock);
+		sleep(1);
+	}
 }
 
 void * writer(void * argv)
 {
-	
+	while(1)
+	{
+		pthread_rwlock_wrlock(&rwlock);
+		data++;
+		printf("父进程写到%d\n", data);
+		pthread_rwlock_unlock(&rwlock);
+		sleep(1);
+	}
 }
 int main()
 {
@@ -20,8 +34,29 @@ int main()
 		perror("pthread_rwlock_init");
 		exit(EXIT_FAILURE);
 	}
+	pthread_t pid_w1;
+	pthread_create(&pid_w1, NULL, writer, NULL);	
+	pthread_t pid_w2;
+	pthread_create(&pid_w2, NULL, writer, NULL);	
+	pthread_t pid_w3;
+	pthread_create(&pid_w3, NULL, writer, NULL);	
 	
+	
+	pthread_t pid_r1;
+	pthread_create(&pid_r1, NULL, reader, NULL);	
+	pthread_t pid_r2;
+	pthread_create(&pid_r2, NULL, reader, NULL);	
+	pthread_t pid_r3;
+	pthread_create(&pid_r3, NULL, reader, NULL);	
 
 	
+	
+
+	pthread_join(pid_w1, NULL);
+	pthread_join(pid_w2, NULL);
+	pthread_join(pid_w3, NULL);
+	pthread_join(pid_r1, NULL);
+	pthread_join(pid_r2, NULL);
+	pthread_join(pid_r3, NULL);
 	pthread_rwlock_destroy(&rwlock);
 }
