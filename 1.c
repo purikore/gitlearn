4f6bff0516ad94f691cb6e2766cd30a23ddbf7fd
liujia <15096318060@163.com>
2020-11-20 10:01:53 +0800

    pthread

--- 1.c
+++ 1.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+pthread_mutex_t pmt;
+void * func(void *argv)
+{
+	int * a = (int *)argv;
+	for(int i = 0; i < 10000000; i++)
+	{
+		pthread_mutex_lock(&pmt);
+		(*a)++;
+		pthread_mutex_unlock(&pmt);
+	}
+	pthread_exit(NULL);
+}
 int main()
 {
-	printf("hello\n");
+	pthread_mutex_t pmt;
+	int flag = pthread_mutex_init(&pmt, NULL);
+	if(flag == -1)
+	{
+		perror("pthread_mutex_init");
+		exit(EXIT_FAILURE);
+	}
+	int *a = (int *)malloc(sizeof(int));
+	*a = 0;
+	pthread_t pid;
+	pthread_create(&pid, NULL, func, (void *)a);
+	pthread_t pid1;
+	pthread_create(&pid1, NULL, func, (void *)a);
+	
+	pthread_join(pid, NULL);
+	pthread_join(pid1, NULL);
+	printf("%d\n",*a);
+	pthread_mutex_destroy(&pmt);
 	return 0;
 }
