6656b3db2d6db38f2f7667f2f7e2269a8d0e608c
liujia <15096318060@163.com>
2020-11-24 13:54:35 +0800

    save

--- pthread_pool.c
+++ pthread_pool.c
@@ -1,2 +1,36 @@
 #include <stdio.h>
-#include <>
+#include <pthread.h>
+#include <stdlib.h>
+#include "pthread_pool.h"
+
+Pool * pool = NULL;
+
+void pthread_pool_init(int max_size)
+{
+	pool = (Pool *)malloc(sizeof(Pool));
+	pthread_mutex_init(&(pool -> pmt), NULL);
+	pthread_cond_init(&(pool -> pct), NULL);
+	
+	pool -> pids = (pthread_t *)malloc(sizeof(pthread_t) * max_size);
+	pool -> queue_head = NULL;
+	
+	pool -> max_pool_size = max_size;
+	pool -> wait_task_size = 0;
+	pool -> shutdown = FALSE;
+	for(int i = 0; i < max_size; i++)
+	{
+		pthread_create(pool -> pid + i, NULL, pthread_pool_running, (void *)i);
+	}
+}
+
+void * pthread_pool_running(void * argv)
+{
+	int * no = (int *)argv;
+	printf("线程0x%x编号%d开始运行\n", pthread_self(), *no);
+	while(1)
+	{
+		pthread_mutex_lock(&(pool -> pmt));
+		if()
+		
+	}
+}
--- pthread_pool.h
+++ pthread_pool.h
@@ -1,11 +1,31 @@
 #ifndef __PTHREAD_POOL_H__
 #define __PTHREAD_POOL_H__
 
+#define boolean unsigned char
+#define TRUE 1
+#define FALSE 0
+/*
+	任务队列
+*/
+typedef struct pthread_pool
+{
+	pthread_mutex_t pmt;
+	pthread_cond_t pct;
+	
+	pthread_t * pids;
+	struct task_queue * queue_head;
+	
+	int max_pool_size;
+	int wait_task_size;
+	boolean shutdown;
+}Pool;
 
-
-
-
-
+typedef struct task_queue
+{
+	void *(*task) (void * argv);
+	void * argv;
+	struct task_queue * next;
+}Queue;
 
 
 #endif
